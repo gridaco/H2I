1f29dd2ad86a46903841f258ddabf7636bc7bc05
softmarshmallow <woojoo@softmarshmallow.com>
2023-04-22 20:45:52 +0900

    rename

---
+++ web/pages/templates/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import React from "react";
+
+export default function Templates() {
+  return (
+    <>
+      <Head>
+        <title>Templates - H2I</title>
+        <meta name="description" content="H2I Templates Library" />
+      </Head>
+    </>
+  );
+}
