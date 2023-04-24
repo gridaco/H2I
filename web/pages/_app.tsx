a5301fece12bf7c1fe85e556ef7a949e500a695f
softmarshmallow <woojoo@softmarshmallow.com>
2023-04-25 00:59:30 +0900

    add gtag

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Script from "next/script";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-RD01DS69BF"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-RD01DS69BF');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
