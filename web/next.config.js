fa9a9b5a266ab467b5c03090e0cb2f6279ed9615
softmarshmallow <woojoo@softmarshmallow.com>
2023-04-25 00:11:05 +0900

    cfg

--- web/next.config.js
+++ web/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["h2i"],
-  rewrites() {
+  async rewrites() {
     const docs = process.env.NEXT_PUBLIC_DOCS_URL || "https://docs.html2.io";
     return [
       {
