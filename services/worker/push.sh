a1638ec5079a76cde6b5edcd0cb38af68fd9e4bf
softmarshmallow <woojoo@softmarshmallow.com>
2023-04-27 03:29:06 +0900

    fix

--- services/worker/push.sh
+++ services/worker/push.sh
@@ -1,24 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
-# Exit on error
 set -e
 
-# Set your AWS region
-REGION="us-west-1"
+# Ensure the AWS CLI is installed
+if ! command -v aws &> /dev/null; then
+  echo "Error: AWS CLI is not installed. Please install it first."
+  exit 1
+fi
 
-# Get the ECR repository URL from the Terraform output
-REPOSITORY_URL=$(terraform output -state=../terraform/terraform.tfstate worker_repository_url)
+# Ensure Docker is installed
+if ! command -v docker &> /dev/null; then
+  echo "Error: Docker is not installed. Please install it first."
+  exit 1
+fi
 
-# Log in to ECR
-aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $REPOSITORY_URL
+# Retrieve ECR repository URL
+REPOSITORY_URL=$(terraform output -json | jq -r '.worker_repository_url.value')
 
 # Build the Docker image
-docker build -t worker .
+docker build -t "${REPOSITORY_URL}" .
 
-# Tag the Docker image with the ECR repository URL
-docker tag worker:latest $REPOSITORY_URL:latest
+# Log in to ECR
+aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin "$(echo ${REPOSITORY_URL} | cut -d/ -f1)"
 
-# Push the Docker image to the ECR repository
-docker push $REPOSITORY_URL:latest
-
-echo "Docker image pushed to ECR repository: $REPOSITORY_URL"
+# Push the Docker image to ECR
+docker push "${REPOSITORY_URL}"
