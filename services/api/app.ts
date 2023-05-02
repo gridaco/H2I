86cafe05b51038eb26520c9afebf10392c491afa
softmarshmallow <woojoo@softmarshmallow.com>
2023-05-02 17:21:44 +0900

    add src support and request validations, allow GET for image request

--- services/api/app.ts
+++ services/api/app.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { image } from "./chrome";
+import { ChromeImageExport, image } from "./chrome";
 import { upload } from "./s3";
 import { nanoid } from "nanoid";
+import assert from "assert";
 
 function logErrors(err, req, res, next) {
   console.error(err.stack);
@@ -25,32 +26,117 @@ app.use(bodyParser.json());
 
 app.use(logErrors);
 
-app.post("/image", async (req, res) => {
-  const { html, url } = req.body;
+app.get("/image", async (req, res) => {
+  const { html, url } = req.query;
+  const id = nanoid();
+
+  // validations
   try {
-    const id = nanoid();
+    assert(html || url, "html or url is required");
+    if (url) {
+      assert(typeof url === "string", "url must be a string");
+      try {
+        new URL(url);
+      } catch (e) {
+        assert.fail("url is not a valid url");
+      }
+    }
+    if (html) {
+      assert(typeof html === "string", "html must be a string");
+    }
+  } catch (e) {
+    console.error(e);
+    res.status(400).json({ error: e.message });
+  }
 
-    const { title, img, userAgent, version } = await image(url);
-
-    const imgurl = await upload(
-      `${id}.png`,
-      // create a buffer from the base64 string
-      Buffer.from(img.replace(/^data:image\/png;base64,/, ""), "base64"),
+  // main
+  try {
+    res.json(
+      await handleImageRequest({
+        mode: url ? "url" : "src",
+        id,
+        url: url as string,
+        src: html as string,
+      }),
     );
-
-    res.json({
-      id: id,
-      title,
-      userAgent,
-      url: imgurl,
-      chrome: {
-        version,
-      },
-    });
   } catch (e) {
     console.error(e);
     res.status(500).json({ error: e.message });
   }
 });
+
+app.post("/image", async (req, res) => {
+  const { html, url, src } = req.body;
+  const id = nanoid();
+
+  // validations
+  try {
+    assert(html || url || src, "html, url or src is required");
+    if (url) {
+      try {
+        new URL(url);
+      } catch (e) {
+        assert.fail("url is not a valid url");
+      }
+    }
+    if (html) {
+      assert(typeof html === "string", "html must be a string");
+    }
+    if (src) {
+      if (typeof src === "object") {
+        assert(src["index.html"], "src must contain index.html");
+      }
+    }
+  } catch (e) {
+    console.error(e);
+    res.status(400).json({ error: e.message });
+  }
+
+  // main
+  try {
+    res.json(
+      await handleImageRequest({
+        mode: url ? "url" : "src",
+        id,
+        url,
+        src: src ?? html,
+      }),
+    );
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: e.message });
+  }
+});
+
+async function handleImageRequest({
+  id,
+  url,
+  src,
+  mode,
+}: {
+  id: string;
+} & ChromeImageExport) {
+  const { title, img, userAgent, version } = await image({
+    mode,
+    url,
+    src,
+  });
+
+  const imgurl = await upload(
+    `${id}.png`,
+    // create a buffer from the base64 string
+    Buffer.from(img.replace(/^data:image\/png;base64,/, ""), "base64"),
+  );
+
+  return {
+    id: id,
+    title,
+    userAgent,
+    url: imgurl,
+    chrome: {
+      version,
+    },
+  };
+}
 
 export { app };
--- services/api/chrome/index.ts
+++ services/api/chrome/index.ts
@@ -1,20 +1,79 @@
-import puppeteer from "puppeteer-core";
+import puppeteer, { PuppeteerLifeCycleEvent } from "puppeteer-core";
 import chromium from "@sparticuz/chromium";
+import assert from "assert";
 
-export async function image(url: string) {
-  const browser = await puppeteer.launch({
+async function getBrowser() {
+  return puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath(),
     headless: chromium.headless,
     ignoreHTTPSErrors: true,
   });
+}
 
+export interface ChromeImageExport {
+  mode: "src" | "url";
+  url?: string;
+  src?: string | { [key: string]: string };
+  waitUntil?: PuppeteerLifeCycleEvent | PuppeteerLifeCycleEvent[];
+}
+
+export async function image({
+  mode,
+  url,
+  src,
+  waitUntil = "networkidle0",
+}: ChromeImageExport) {
+  const browser = await getBrowser();
   const page = await browser.newPage();
+  const warnings = [];
 
-  await page.goto(url, {
-    waitUntil: "networkidle0",
-  });
+  switch (mode) {
+    case "src":
+      assert(src, "src is required");
+      if (typeof src === "object") {
+        const loads = [];
+        for (const [key, value] of Object.entries(src)) {
+          if (key.endsWith(".css")) {
+            loads.push(
+              page.addStyleTag({
+                content: value,
+              }),
+            );
+          } else if (key.endsWith(".js")) {
+            loads.push(
+              page.addScriptTag({
+                content: value,
+              }),
+            );
+          } else if (key.endsWith(".html")) {
+            loads.push(
+              page.setContent(value, {
+                waitUntil,
+              }),
+            );
+          } else {
+            warnings.push(
+              `Unknown key ${key} with value ${value} in src object`,
+            );
+          }
+        }
+        await Promise.all(loads);
+      }
+      if (typeof src === "string") {
+        await page.setContent(src as string, {
+          waitUntil,
+        });
+      }
+      break;
+    case "url":
+      assert(url, "url is required");
+      await page.goto(url, {
+        waitUntil,
+      });
+      break;
+  }
 
   const promises = [
     //
@@ -34,6 +93,7 @@ export async function image(url: string) {
   await page.close();
 
   return {
+    warnings,
     version,
     userAgent,
     title,
